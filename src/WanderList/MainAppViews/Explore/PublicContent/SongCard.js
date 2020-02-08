578f3e3d96623cf83fe16fdd8d5bd358e902441f
Taylor Harrison Carroll <taylorhcarroll1@gmail.com>
2020-02-08 16:10:02 -0600

    adding search functionality

---
+++ src/WanderList/MainAppViews/Explore/PublicContent/SongCard.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+// import { Button } from "reactstrap";
+import "./Searchfield.css";
+
+export class SongCard extends Component {
+  // handleInput = evt => {
+  //   console.log(evt.target.value);
+  //   const stateToChange = {};
+  //   stateToChange[evt.target.id] = evt.target.value;
+  //   this.setState(stateToChange);
+  //   console.log(stateToChange);
+  // };
+
+  addSongToPlaylist = event => {
+    console.log("props", this.props);
+    // const playlistId = sessionStorage.getItem("currentPlaylistId")
+    // const currentPlaylistId = sessionStorage.getItem("PlaylistId");
+    const songInfo = {
+      songName: this.props.track.name,
+      albumName: this.props.track.album.name,
+      artistName: this.props.track.artists[0].name,
+      song_uri: [this.props.track.uri],
+      song_id: this.props.track.id,
+      spotifyPlaylistId: this.props.currentPlaylistId,
+      playlistId: this.props.playlistId,
+    };
+    this.props.addSongToSpotify(songInfo);
+    console.log(songInfo);
+  };
+
+  render() {
+    return (
+      <div className="songSearch">
+        <div className="singleTrack">
+          <div>
+            {this.props.track.name} by {this.props.track.artists[0].name}
+            {"  "}
+            <button
+              size="sm"
+              color="info"
+              className="add"
+              onClick={this.addSongToPlaylist}
+            >
+              +
+            </button>
+          </div>
+        </div>
+        <hr className="hrLineSong" />
+      </div>
+    );
+  }
+}
+
+export default SongCard;
