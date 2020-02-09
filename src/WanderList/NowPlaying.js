6a2f30897b0d70e3079e590af0119e8716dbfcd2
Taylor Harrison Carroll <taylorhcarroll1@gmail.com>
2020-02-09 17:07:47 -0600

    fixed another now playing issue

--- src/WanderList/NowPlaying.js
+++ src/WanderList/NowPlaying.js
@@ -7,74 +7,87 @@ class NowPlaying extends Component {
     constructor() {
         super();
         this.state = {
-          token: null,
-          item: {
-            album: {
-              images: [{ url: "" }]
+            token: null,
+            item: {
+                album: {
+                    images: [{ url: "" }]
+                },
+                name: "",
+                artists: [{ name: "" }],
+                duration_ms: 0,
             },
-            name: "",
-            artists: [{ name: "" }],
-            duration_ms: 0,
-          },
-          is_playing: "Paused",
-          progress_ms: 0
+            is_playing: "Paused",
+            progress_ms: 0
         };
         this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
-      }
-      getCurrentlyPlaying(token) {
+    }
+    getCurrentlyPlaying(token) {
         // Make a call using the token
         $.ajax({
-          url: "https://api.spotify.com/v1/me/player",
-          type: "GET",
-          beforeSend: (xhr) => {
-            xhr.setRequestHeader("Authorization", "Bearer " + token);
-          },
-          success: (data) =>{
-           {
-            console.log("data", data);
-            this.setState({
-              username: data.id,
-              item: data.item,
-              is_playing: data.is_playing,
-              progress_ms: data.progress_ms,
-            });
-          }
-        },
-        error: () => {
-            this.setState({
-                item: "Nothing",
-                is_playing: "Nothing is playing",
-              });
-        }
+            url: "https://api.spotify.com/v1/me/player",
+            type: "GET",
+            beforeSend: (xhr) => {
+                xhr.setRequestHeader("Authorization", "Bearer " + token);
+            },
+            success: (data) => {
+                {
+                    console.log("data", data);
+                    if (data) {
+                        this.setState({
+                            username: data.id,
+                            item: data.item,
+                            is_playing: data.is_playing,
+                            progress_ms: data.progress_ms,
+                        });
+                    } else {
+                        this.setState({
+                            item: "Nothing",
+                            is_playing: "Nothing is playing",
+                            progress_ms: "1",
+                            item: {
+                                album: {
+                                    images: [{ url: "/images/noSongPlaying.svg" }]
+                                },
+                            }});
+                    }
+                }
+            },
+            error: () => {
+                this.setState({
+                    item: "Nothing",
+                    is_playing: "Nothing is playing",
+                });
+            }
         });
-      }
+    }
     componentDidMount() {
         // Set token
         let _token = hash.access_token;
 
         if (_token) {
-          // Set token
-          this.setState({
-            token: _token
-          });
-          this.getCurrentlyPlaying(_token);
+            // Set token
+            this.setState({
+                token: _token
+            });
+            this.getCurrentlyPlaying(_token);
         }
-      }
+    }
 
     render() {
-		return (
-<>
-<h3>This is the now playing section</h3>
-             {this.state.token && (
-              <Player
-                item={this.state.item}
-                is_playing={this.state.is_playing}
-                progress_ms={this.progress_ms}
-              />
-            )}
-</>
+        return (
+            <>
+                <h3>This is the now playing section</h3>
+                {this.state.token && (
+                    <Player
+                        item={this.state.item}
+                        is_playing={this.state.is_playing}
+                        progress_ms={this.progress_ms}
+                    />
+                )}
+            </>
         )
 
-}}
+    }
+}
 
 export default NowPlaying;
